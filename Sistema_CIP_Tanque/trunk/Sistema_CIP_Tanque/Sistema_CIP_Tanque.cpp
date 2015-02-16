b70a7cab190125ca6a338f620f968ee7791a9213
brclauss@gmail.com <brclauss@gmail.com@8233e7ce-1ae3-6343-552e-f900e6b63d09>
2015-02-16 17:26:59 +0000

    Creación del proyecto

---
+++ Sistema_CIP_Tanque/trunk/Sistema_CIP_Tanque/Sistema_CIP_Tanque.cpp
@@ -0,0 +1,17 @@
+/*
+ * Sistema_CIP_Tanque.cpp
+ *
+ * Created: 16/02/2015 14:04:50
+ *  Author: Bruno
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
